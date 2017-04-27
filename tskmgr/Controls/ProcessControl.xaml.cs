089c3f36ff3ba2b75313b11f13ce45e1814a6d3f
infyhr <infy@cybershade.org>
2017-04-27 15:25:22 +0200

    Initial push

---
+++ tskmgr/Controls/ProcessControl.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+
+namespace tskmgr.Controls
+{
+    /// <summary>
+    /// Interaction logic for ProcessControl.xaml
+    /// </summary>
+    public partial class ProcessControl : UserControl, INotifyPropertyChanged
+    {
+        public ObservableCollection<ProcessList> ProcessCollection { get; set; }
+        private int? selectedIndex = 0;
+
+        private Process _Process = new Process();
+        private PerformanceCounter cpuLoad = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+        public ProcessControl()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            this.ProcessCollection = _Process.GetProcessList();
+
+            // Stvaranje nove niti za asinkrono ažuriranje sadržaja
+            AsyncUpdate();
+        }
+
+        private async void AsyncUpdate()
+        {
+            await Task.Factory.StartNew(() => this.UpdateData(), TaskCreationOptions.LongRunning);
+        }
+
+        private void UpdateData()
+        {
+            // http://stackoverflow.com/questions/19558644/update-an-observablecollection-from-another-collection
+            while (true)
+            {
+                // Pozovi UI nit da očisti sve trenutne procese
+                App.Current.Dispatcher.BeginInvoke((Action)delegate {
+                    this.ProcessCollection.Clear();
+                });
+
+                // Dohvati nove procese
+                var newProcesses = _Process.GetProcessList();
+                // Pozovi UI nit da doda svaki unos, jedan po jedan -- Smanjuje blokiranje GUI niti na koju se offloada cijela kolekcija.
+                foreach (var p in newProcesses)
+                {
+                    App.Current.Dispatcher.BeginInvoke((Action)delegate {
+                        this.ProcessCollection.Add(p);
+                    });
+                }
+
+                App.Current.Dispatcher.BeginInvoke((Action)delegate {
+                    // Ažuriraj selektirane pozive
+                    DataGrid.SelectedIndex = (int)this.selectedIndex;
+
+                    // Ažuriraj CPU i RAM Usage
+                    this.Processes.Text = String.Format("Processes: {0}%", (int)_Process.GetTotalCPUUsage());
+                    this.MemoryUsage.Text = String.Format("Available Memory: {0}MB", (int)_Process.GetTotalMemoryUsage());
+                    this.Processes.Text = String.Format("Processes: {0}", this.ProcessCollection.Count.ToString());
+                });
+
+                Task.Delay(5000).Wait();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(DataGrid.SelectedIndex != -1) this.selectedIndex = DataGrid.SelectedIndex;
+        }
+    }
+}
---
+++ tskmgr/Process.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace tskmgr
+{
+    public class ProcessList : INotifyPropertyChanged
+    {
+        private int processId;
+        public int ProcessId
+        {
+            get { return this.processId; }
+            set
+            {
+                this.processId = value;
+                this.NotifyPropertyChanged("ProcessId");
+            }
+        }
+        private string processName;
+        public string ProcessName {
+            get { return this.processName; }
+            set
+            {
+                this.processName = value;
+                this.NotifyPropertyChanged("ProcessName");
+            }
+        }
+        private System.TimeSpan processTime;
+        public System.TimeSpan ProcessTime {
+            get { return this.processTime; }
+            set
+            {
+                this.processTime = value;
+                this.NotifyPropertyChanged("ProcessTime");
+            }
+        }
+        private int threadCount;
+        public int ThreadCount {
+            get { return this.threadCount; }
+            set
+            {
+                this.threadCount = value;
+                this.NotifyPropertyChanged("ThreadCount");
+            }
+        }
+        private long workingSet64;
+        public long WorkingSet64 {
+            get { return this.workingSet64; }
+            set
+            {
+                this.workingSet64 = value;
+                this.NotifyPropertyChanged("WorkingSet64");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName) {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+    }
+
+    public class Process
+    {
+        PerformanceCounter CPUCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        PerformanceCounter MEMCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+        public Process() { }
+
+        public float GetTotalCPUUsage()
+        {
+            return this.CPUCounter.NextValue();
+        }
+
+        public float GetTotalMemoryUsage()
+        {
+            return this.MEMCounter.NextValue();
+        }
+
+        public ObservableCollection<ProcessList> GetProcessList()
+        {
+            ObservableCollection<ProcessList> returnList = new ObservableCollection<ProcessList>();
+
+            System.Diagnostics.Process[] localProcesses = System.Diagnostics.Process.GetProcesses();
+            foreach (var p in localProcesses)
+            {
+                ProcessList singleProcess = new ProcessList();
+                singleProcess.ProcessId = p.Id;
+                singleProcess.ProcessName = p.ProcessName;
+                singleProcess.ProcessTime = (p.Id != 0) ? p.TotalProcessorTime : new System.TimeSpan(0, 0, 0);
+                singleProcess.ThreadCount = p.Threads.Count;
+                singleProcess.WorkingSet64 = p.WorkingSet64;
+
+                returnList.Add(singleProcess);
+            }
+
+            return returnList;
+        }
+    }
+}
